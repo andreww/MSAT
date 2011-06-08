eb6ea2338715c35209afadb6f373434a6ee9e96e
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-06-08 15:49:09 +0100

    Interpolator for elastic constants.

---
+++ tests/test_MS_interpolate.m
@@ -0,0 +1,62 @@
+function test_suite = test_MS_interpolate
+initTestSuite;
+end
+
+function test_MS_interpolate_angs_ortho
+    % Test the interpolator works for constant
+    % matrices rotated about axies
+ 
+    [C, r] = MS_elasticDB('olivine');
+    C90x = MS_rot3(C, 90, 0, 0);
+    C45x = MS_rot3(C, 45, 0, 0);
+    assertElementsAlmostEqual(MS_interpolate(C, C90x, 0.5), C45x);
+    assertElementsAlmostEqual(MS_interpolate(C90x, C, 0.5), C45x);
+    C40y = MS_rot3(C, 0, 40, 0);
+    C30y = MS_rot3(C, 0, 30, 0);
+    C31y = MS_rot3(C, 0, 31, 0);
+    C35y = MS_rot3(C, 0, 35, 0);
+    C39y = MS_rot3(C, 0, 39, 0);
+    assertElementsAlmostEqual(MS_interpolate(C30y, C40y, 0.5), C35y);
+    assertElementsAlmostEqual(MS_interpolate(C30y, C40y, 0.1), C31y);
+    assertElementsAlmostEqual(MS_interpolate(C30y, C40y, 0.9), C39y);
+    assertElementsAlmostEqual(MS_interpolate(C40y, C30y, 0.1), C39y);
+    assertElementsAlmostEqual(MS_interpolate(C40y, C30y, 0.5), C35y);
+    assertElementsAlmostEqual(MS_interpolate(C40y, C30y, 0.9), C31y);
+    C10z = MS_rot3(C, 0, 0, 10);
+    C110z = MS_rot3(C, 0, 0, 110);
+    C60z = MS_rot3(C, 0, 0, 60);
+    assertElementsAlmostEqual(MS_interpolate(C10z, C110z, 0.5), C60z);
+    assertElementsAlmostEqual(MS_interpolate(C110z, C10z, 0.5), C60z);
+    assertElementsAlmostEqual(MS_interpolate(C110z, C10z, 0.0), C110z);
+    assertElementsAlmostEqual(MS_interpolate(C110z, C10z, 1.0), C10z);
+end
+
+function test_MS_interpolate_angs_triclin
+    % Test the interpolator works for constant
+    % matrices rotated about axies
+ 
+    [C, r] = MS_elasticDB('albite');
+    C90x = MS_rot3(C, 90, 0, 0);
+    C45x = MS_rot3(C, 45, 0, 0);
+    assertElementsAlmostEqual(MS_interpolate(C, C90x, 0.5), C45x);
+    assertElementsAlmostEqual(MS_interpolate(C90x, C, 0.5), C45x);
+    C40y = MS_rot3(C, 0, 40, 0);
+    C30y = MS_rot3(C, 0, 30, 0);
+    C31y = MS_rot3(C, 0, 31, 0);
+    C35y = MS_rot3(C, 0, 35, 0);
+    C39y = MS_rot3(C, 0, 39, 0);
+    assertElementsAlmostEqual(MS_interpolate(C30y, C40y, 0.5), C35y);
+    assertElementsAlmostEqual(MS_interpolate(C30y, C40y, 0.1), C31y);
+    assertElementsAlmostEqual(MS_interpolate(C30y, C40y, 0.9), C39y);
+    assertElementsAlmostEqual(MS_interpolate(C40y, C30y, 0.1), C39y);
+    assertElementsAlmostEqual(MS_interpolate(C40y, C30y, 0.5), C35y);
+    assertElementsAlmostEqual(MS_interpolate(C40y, C30y, 0.9), C31y);
+    C10z = MS_rot3(C, 0, 0, 10);
+    C110z = MS_rot3(C, 0, 0, 110);
+    C60z = MS_rot3(C, 0, 0, 60);
+    assertElementsAlmostEqual(MS_interpolate(C10z, C110z, 0.5), C60z);
+    assertElementsAlmostEqual(MS_interpolate(C110z, C10z, 0.5), C60z);
+    assertElementsAlmostEqual(MS_interpolate(C110z, C10z, 0.0), C110z);
+    assertElementsAlmostEqual(MS_interpolate(C110z, C10z, 1.0), C10z);
+end
+
