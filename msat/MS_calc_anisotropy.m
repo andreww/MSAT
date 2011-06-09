2ccf6ad75f5c583c030021b757d47e75b9c8e50b
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-06-09 13:50:41 +0100

    Add Ledbetter and Miglion' shear wave anisotropy

--- msat/MS_calc_anisotropy.m
+++ msat/MS_calc_anisotropy.m
@@ -4,7 +4,7 @@
 %
 % Calculate the degree of ansiotropy of an elasticity matrix. 
 %
-% [ uA, ... ] = MS_calc_anisotropy( C )
+% [ uA, ... ] = MS_calc_anisotropy( C, ... )
 %
 % Usage: 
 %     [ uA ] = MS_calc_anisotropy( C )                    
@@ -13,32 +13,51 @@
 %         uA is zero for an isotropic case and increses for incresing 
 %         anisotropy.
 %
-%     [ uA, zA ] = MS_calc_anisotropy( C )
+%     [ uA, lmA ] = MS_calc_anisotropy( C )
+%         Also return the general measure of anisotropy proposed by
+%         Ledbetter and Miglion (2006). This the the ratio of the fastest
+%         and slowest squared shear wave velocity over all propogation
+%         and polarization directions. Equal to one in the isotropic case,
+%         increses with incresing anisotropy.
+%
+%     [ uA, lmA, zA ] = MS_calc_anisotropy( C )
 %         Also return the Zenner (1948) measure of anisotropy. This is
 %         only valid for cubic crystals (NaN is returned if C does not 
 %         represent a cubic crystal). zA is 1 for an isotropic case and
 %         increses or decreses with incresing anisotropy.
 %
-%     [ uA, zA, cbA ] = MS_calc_anisotropy( C )
+%     [ uA, lmA, zA, cbA ] = MS_calc_anisotropy( C )
 %         Also return the Chung-Buessem (1967) anisotropy index. This
 %         is a single valued measure of anisotropy derived from zA. Like
 %         uA, this is zero for an isotropic case and increses for incresing 
 %         anisotropy. Only valid for matricies representing cubic crystals.
+%
+%     [ uA, lmA, ... ] = MS_calc_anisotropy( C, n )
+%         Set the number of random directions to sample for the calculation
+%         of lmA. Defaults to 1000, which seems to give results accurate to
+%         two decimal places. Ledbetter and Miglion (2006) use 10000 which 
+%         gives results reproducable to three decimal places and a 
+%         noticable slow down.
 %
 % Notes:
 %     These measures of anisotropy are independent of orientation. However,
 %     the test for cubic symmetry assumes the matrix is in an ideal
 %     orention. Use MS_AXES to reorentate the imput matrix for the general 
 %     case. MS_NORMS can be used to provide an alternate measure of
-%     anisotropy.
+%     anisotropy. Ledbetter and Miglion (2006) claim lmA is identcal to zA
+%     for cubic cases but Ranganathan and Ostoja-Starzewski (2008) point 
+%     out cases where zA < 1 while lmA > 1 by construction. 
 %
 % References:
-%
 %     Zenner, C. (1948) Elasticity and Anelasticiy of Metals. University 
 %     of Chicago. 
 %
 %     Chung, D. H. and W. R. Buessem (1967) Journal of Applied Physics 
 %     vol.38 p.5 
+%
+%     Ledbetter, H. and A. Miglion (2006) "A general elastic-anisotropy 
+%     measure" Journal of Applied Physics vol.100 art.num.063516
+%     http://dx.doi.org/10.1063/1.2338835
 %
 %     Shivakumar, I. and M. Ostoja-Starzewski (2008) "Universal Elastic
 %     Anisotropy Index" Physical Review Letters vol.101 art.num.055504.
@@ -48,9 +67,17 @@
 
 % (C) James Wookey and Andrew Walker, 2011
 
-function [ uA, zA, cbA ] = MS_calc_anisotropy( C )
+function [ uA, lmA, zA, cbA] = MS_calc_anisotropy( C, varargin )
 
+    numprop = 1000;
+    % process the optional arguments
+    if nargin==2
+        numprop = varargin{1};
+    end
+    
     uA = UniversalAnisotropyIndex(C);
+    if (nargout == 1), return, end % Skip lmA if we don't need it
+    lmA = LedbetterAnisotropyIndex(C, numprop);
     if (isCubic( C ))
         zA = ZenerAnisotropyIndex(C);
         cbA = (3*(zA-1)^2) / (3*(zA-1)^2 + 25*zA);
@@ -94,3 +121,22 @@ function uA = UniversalAnisotropyIndex( C )
     uA = (5*(G_v/G_r) + (B_v/B_r)) - 6;
     
 end
+
+function lmA = LedbetterAnisotropyIndex( C, n )
+
+    points = randomSphere(n);
+    % Density should not matter - we ratio velocities.
+    [~,~,vs1,vs2,~] = MS_phasevels(C,3000,points(:,2),points(:,1));
+    lmA = (max(vs1))^2 / (min(vs2))^2; 
+    
+end 
+
+function points = randomSphere( n )
+    % n randomly distributed points on a sphere (polar co-ordinates, 
+    % in degrees). Points(:,1) are azimuths, points(:,2) are inclinations.
+    points = rand(n,2);
+    points(:,1) = points(:,1)*pi*2.0;
+    points(:,2) = acos(2.0*points(:,2)-1);
+    points = points*(180.0/pi);
+    points(:,2) = points(:,2) - 90.0;
+end 
