f85fdc02e4c0511839a6bf38fd58922974572635
James Wookey <j.wookey@bristol.ac.uk>
2011-07-01 15:36:13 +1000

    Minor changes to test_MS_axes

--- tests/test_MS_axes.m
+++ tests/test_MS_axes.m
@@ -12,7 +12,7 @@ function test_MS_axes_ortho
       b1 = rand.*360 ; b2 = rand.*360 ;
       g1 = rand.*360 ; g2 = rand.*360 ;
       
-      fprintf('Rotations: [%6.1f %6.1f %6.1f] cf. [%6.1f %6.1f %6.1f]\n',a1,b1,g1,a2,b2,g2)
+      fprintf('Rotations: [%6.1f %6.1f %6.1f] cf. [%6.1f %6.1f %6.1f]\n',a1,b1,g1,a2,b2,g2) ;
       
       C1 = MS_rot3(C,a1,b1,g1) ;
       C2 = MS_rot3(C,a2,b2,g2) ;
@@ -38,7 +38,7 @@ function test_MS_axes_triclin_weak
       b1 = rand.*360 ; b2 = rand.*360 ;
       g1 = rand.*360 ; g2 = rand.*360 ;
       
-      fprintf('Rotations: [%6.1f %6.1f %6.1f] cf. [%6.1f %6.1f %6.1f]\n',a1,b1,g1,a2,b2,g2)
+      fprintf('Rotations: [%6.1f %6.1f %6.1f] cf. [%6.1f %6.1f %6.1f]\n',a1,b1,g1,a2,b2,g2) ;
       
       C1 = MS_rot3(C,a1,b1,g1) ;
       C2 = MS_rot3(C,a2,b2,g2) ;
@@ -59,7 +59,7 @@ function test_MS_axes_triclin_strong
       b1 = rand.*360 ; b2 = rand.*360 ;
       g1 = rand.*360 ; g2 = rand.*360 ;
       
-      fprintf('Rotations: [%6.1f %6.1f %6.1f] cf. [%6.1f %6.1f %6.1f]\n',a1,b1,g1,a2,b2,g2)
+      fprintf('Rotations: [%6.1f %6.1f %6.1f] cf. [%6.1f %6.1f %6.1f]\n',a1,b1,g1,a2,b2,g2) ;
       
       C1 = MS_rot3(C,a1,b1,g1) ;
       C2 = MS_rot3(C,a2,b2,g2) ;
