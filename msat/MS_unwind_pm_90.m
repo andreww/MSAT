e85e8d9450c533b3e2c3e25f9df9d481b50a7342
James Wookey <j.wookey@bristol.ac.uk>
2011-07-04 09:35:42 +1000

    Unwind codes
    * Removed unused unwind codes.
    
    * Code and comment tidying on MS_unwind_pm90 (only one which is used).

--- msat/MS_unwind_pm_90.m
+++ msat/MS_unwind_pm_90.m
@@ -1,13 +1,17 @@
 %  UNWIND_PM_90 - Unwind an angle until it is between -90 and 90 degrees
 %
-%  [angle] = unwind_pm_90(angle_in)
+% // Part of MSAT - The Matlab Seismic Anisotropy Toolkit //
+%
+% [angle] = MS_unwind_pm_90(angle_in)
 %
 %  This (obviously) assumes a 180 degree periodicity. Angle can be a scalar
 %  or a vector. 
 %
-%  Created by James Wookey on 2007-05-08.
-%
-function [angle] = unwind_pm_90(angle_in)
+% See also: 
+
+% (C) James Wookey and Andrew Walker, 2011
+
+function [angle] = MS_unwind_pm_90(angle_in)
 %===============================================================================
    angle = angle_in ;
    for i=1:1000 % should never reach this
