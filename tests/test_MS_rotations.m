82aef67f671601c447561fdd4ad7bfdb6b7b4546
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-07-19 19:54:39 +0100

    Fill out test cases for MS_rot3

--- tests/test_MS_rotations.m
+++ tests/test_MS_rotations.m
@@ -17,3 +17,97 @@ function test_MS_rotations_triclinic
 
 end
 
+function test_MS_rot_3_scalar_vector
+
+    C_in = [  49.67  13.18   13.18   000.0   000.0   000.0
+              13.18  49.67   13.18   000.0   000.0   000.0
+              13.18  13.18   49.67   000.0   000.0   000.0
+              000.0  000.0   000.0   12.78   000.0   000.0
+              000.0  000.0   000.0   000.0   12.78   000.0
+              000.0  000.0   000.0   000.0   000.0   12.78];
+    Cs_in = zeros(6,6,3);
+    Cs_in(:,:,1) = C_in;
+    Cs_in(:,:,2) = C_in;
+    Cs_in(:,:,3) = C_in;
+    a = 90.0;
+    b = 0.0;
+    g = 0.0;
+    as = [a ; a; a];
+    bs = [b ; b ; b];
+    gs = [g ; g ; g];
+    
+    % Scalar test
+    assertElementsAlmostEqual(C_in, MS_rot3(C_in, a, b, g))
+    % Vecor tests
+    assertElementsAlmostEqual(Cs_in, MS_rot3(Cs_in, a, b, g))
+    assertElementsAlmostEqual(Cs_in, MS_rot3(C_in, as, bs, gs))
+    assertElementsAlmostEqual(Cs_in, MS_rot3(Cs_in, as, bs, gs))
+end
+
+function test_MS_rot_3_errors
+
+    C_in = [  49.67  13.18   13.18   000.0   000.0   000.0
+              13.18  49.67   13.18   000.0   000.0   000.0
+              13.18  13.18   49.67   000.0   000.0   000.0
+              000.0  000.0   000.0   12.78   000.0   000.0
+              000.0  000.0   000.0   000.0   12.78   000.0
+              000.0  000.0   000.0   000.0   000.0   12.78];
+    badC_in = [  49.67  13.18   13.18   000.0   000.0   000.0
+                 31.18  49.67   13.18   000.0   000.0   000.0
+                 13.18  13.18   49.67   000.0   000.0   000.0
+                 000.0  000.0   000.0   12.78   000.0   000.0
+                 000.0  000.0   000.0   000.0   12.78   000.0
+                 000.0  000.0   000.0   000.0   000.0   12.78];
+    a = 90.0;
+    b = 0.0;
+    g = 0.0;
+    
+    f = @()MS_rot3(badC_in, a, b, g);
+    assertExceptionThrown(f, 'MS:ROT3BadInputMatrix')
+        
+    f = @()MS_rot3(C_in, a, b, g, 'order',[1 1 1 1]);
+    assertExceptionThrown(f, 'MS:ROT3:BadOrder')
+    
+    f = @()MS_rot3(C_in, a, b, g, 'order',[1 2 4]);
+    assertExceptionThrown(f, 'MS:ROT3:BadOrder')
+    
+    f = @()MS_rot3(C_in, a, b, g, 'bob',[1 2 3]);
+    assertExceptionThrown(f, 'MS:ROT3:UnknownOption')
+    
+   
+end
+
+function test_MS_rot_3_vecerrors
+
+    C_in = [  49.67  13.18   13.18   000.0   000.0   000.0
+              13.18  49.67   13.18   000.0   000.0   000.0
+              13.18  13.18   49.67   000.0   000.0   000.0
+              000.0  000.0   000.0   12.78   000.0   000.0
+              000.0  000.0   000.0   000.0   12.78   000.0
+              000.0  000.0   000.0   000.0   000.0   12.78];
+    Cs_in = zeros(6,6,3);
+    Cs_in(:,:,1) = C_in;
+    Cs_in(:,:,2) = C_in;
+    Cs_in(:,:,3) = C_in;
+    Cs_in(:,:,4) = C_in;
+    a = 90.0;
+    b = 0.0;
+    g = 0.0;
+    as = [a ; a; a];
+    bs = [b ; b ; b];
+    gs = [g ; g ; g];
+    
+    % 4 elastic matices and 3 rotation objects...
+    f = @()MS_rot3(Cs_in, as, bs, gs);
+    assertExceptionThrown(f, 'MS:ListsMustMatch')
+    
+    % 4 elastic matices and 3 rotation objects...
+    f = @()MS_rot3(Cs_in, as, b, gs);
+    assertExceptionThrown(f, 'MS:ListsMustMatch')
+    
+    % 4 elastic matices and 3 rotation objects...
+    f = @()MS_rot3(Cs_in, as, bs, g);
+    assertExceptionThrown(f, 'MS:ListsMustMatch')
+    
+end
+
