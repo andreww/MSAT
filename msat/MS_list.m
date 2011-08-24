c0fd2dc0fae3b85d285ae8342dd77d42be5c4c15
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-08-24 14:13:39 +0100

    Docs for MS_list.

--- msat/MS_list.m
+++ msat/MS_list.m
@@ -1,10 +1,31 @@
+% MS_LIST - Print elasticity matrix (in 'list' format)
+%
+% // Part of MSAT - The Matlab Seismic Anisotropy Toolkit //
+%
 % CIJ_list(C,rho)
+%
+% Usage: 
+%     CIJ_list(C,rho)
+%         For a given 6x6 elasticity matrix, C, and density, rho, print 
+%         the constants.
+%
+%  Notes: 
+%    The 'list' format consists of three numbers per line. The first two 
+%    are integers representing the location in the elasticity matrix in
+%    Voigt notation, the third is the value of the elastic constant. After
+%    the 36 lines of elastic constants a line containing the density is
+%    printed with dummy indices '7 7'. This function exists to allow 
+%    MSAT to be interfaced with legacy code.
+%
 
-function MS_list(C,rho);
+% (C) James Wookey and Andrew Walker, 2011
 
-for i=1:6
-   for j=i:6
-      fprintf('%1i %1i %e\n',i,j,C(i,j))
-   end
-end   
-fprintf('%1i %1i %f\n',7,7,rho)
+function MS_list(C,rho)
+
+    for i=1:6
+       for j=i:6
+          fprintf('%1i %1i %e\n',i,j,C(i,j))
+       end
+    end   
+    fprintf('%1i %1i %f\n',7,7,rho)
+end
