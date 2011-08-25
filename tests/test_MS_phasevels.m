573cf20c506e2d3eaee6c0509974843d2313e1ac
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-08-25 17:11:23 +0100

    Basic test of phasevels.
    Do we get Figure 3 of Mainprice's review correct. Added
    stishovite to the elasticDB to check!

---
+++ tests/test_MS_phasevels.m
@@ -0,0 +1,36 @@
+function test_suite = test_MS_phasevels
+initTestSuite;
+end
+
+function test_MS_phasevels_stishovite_graph
+
+    % Check we get the same results as those given in Mainprices review 
+    % (Figure 3).
+    
+    
+    [C, rh] = MS_elasticDB('stishovite');
+    
+    % [001], symmetry axis
+    [pol, avs, vs1, vs2, vp] = MS_phasevels(C, rh, 90, 0);
+    
+    assert(vs1==vs2, 'VS1 ne VS2'); % By symmetry
+    assert(avs==0, 'avs ne 0'); % By symmetry 
+    assert((vp-13.5)^2<0.1^2, 'vp wrong'); % From graph
+    assert((vs1-7.7)^2<0.1^2, 'vs wrong'); % From graph
+    assert(isnan(pol), 'pol is wrong')
+    
+    % [100]
+    [~, ~, vs1, vs2, vp] = MS_phasevels(C, rh, 0, 0);
+    
+    assert((vp-10.2)^2<0.1^2, 'vp wrong'); % From graph
+    assert((vs1-8.4)^2<0.1^2, 'vs wrong'); % From graph
+    assert((vs2-7.7)^2<0.1^2, 'vs wrong'); % From graph
+    
+    % [010]
+    [~, ~, vs1, vs2, vp] = MS_phasevels(C, rh, 0, 90);
+    
+    assert((vp-10.2)^2<0.1^2, 'vp wrong'); % From graph
+    assert((vs1-8.4)^2<0.1^2, 'vs wrong'); % From graph
+    assert((vs2-7.7)^2<0.1^2, 'vs wrong'); % From graph
+
+end 
