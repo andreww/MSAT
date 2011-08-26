844845234ef03ac1046cadb936c93ab6f24d549a
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-08-26 09:34:12 +0100

    Tests for MS_polyaverage
    Do we get stishovite right? No error cases to worry about.
    Also fix typo in output argument list in documentation

---
+++ tests/test_MS_polyaverage.m
@@ -0,0 +1,19 @@
+function test_suite = test_MS_polyaverage
+initTestSuite;
+end
+
+function test_MS_polyaverage_stishovite
+
+
+   % Do we get the same numbers as WEIDNER et al. table 5?
+   [C, rh] = MS_elasticDB('stishovite');
+   [ K_vrh, G_vrh, K_v, G_v, K_r, G_r ] = MS_polyaverage(C);
+   assertElementsAlmostEqual([K_vrh], [316], 'absolute', 0.5);
+   assertElementsAlmostEqual([G_vrh], [220], 'absolute', 0.5);
+   assertElementsAlmostEqual([K_r], [308], 'absolute', 0.5);
+   assertElementsAlmostEqual([G_r], [208], 'absolute', 0.5);
+   assertElementsAlmostEqual([K_v], [324], 'absolute', 0.5);
+   assertElementsAlmostEqual([G_v], [232], 'absolute', 0.5);   
+end
+
+% No error cases to check!
