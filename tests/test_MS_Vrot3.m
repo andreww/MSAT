56776bbccf21ac7d2ceafa5efe336693dddc7aaa
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-10-03 14:03:08 +0100

    Fix up tests for Vrot3

--- tests/test_MS_Vrot3.m
+++ tests/test_MS_Vrot3.m
@@ -6,9 +6,9 @@ function test_MS_Vrot3_simple
 
   V = [1 0 0];
   assertElementsAlmostEqual(MS_Vrot3(V, 0.0, 0.0, 0.0), V); 
-  assertElementsAlmostEqual(MS_Vrot3(V', 0.0, 0.0, 0.0), V);
-  assertElementsAlmostEqual(MS_Vrot3(V, 180.0, 0.0, 0.0), [-1 0 0]); 
-  assertElementsAlmostEqual(MS_Vrot3(V', 180.0, 0.0, 0.0), [-1 0 0]);
+  assertElementsAlmostEqual(MS_Vrot3(V', 0.0, 0.0, 0.0), V');
+  assertElementsAlmostEqual(MS_Vrot3(V, 0.0, 180.0, 0.0), [-1 0 0]); 
+  assertElementsAlmostEqual(MS_Vrot3(V', 0.0, 180.0, 0.0), [-1 0 0]');
 end
 
 function test_MS_Vrot3_errors
@@ -21,7 +21,7 @@ function test_MS_Vrot3_errors
     f = @()MS_Vrot3(V, 0.0, 0.0, 0.0, 'bob', [1 2 3]);
     assertExceptionThrown(f, 'MS:VROT3:UnknownOption')
 
-    f = @()MS_Vrot3([1 0 0 0], 0.0, 0.0, 0.0, 'bob', [1 2 3]);
+    f = @()MS_Vrot3([1 0 0 0], 0.0, 0.0, 0.0);
     assertExceptionThrown(f, 'MS:VROT3BadInputMatrix')
 
 end
