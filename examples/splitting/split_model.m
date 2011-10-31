9bb29efaa1e46de4aa89690170575f7a22cebe30
James Wookey <j.wookey@bristol.ac.uk>
2011-10-31 18:30:25 +0000

    Continued updating
    - Moved all to subds

---
+++ examples/splitting/split_model.m
@@ -0,0 +1,132 @@
+% SPLIT_MODEL.M - Example script modelling shear-wave splitting variation with
+%                 backazimuth. 
+%
+% This script demonstrates simple shear-wave splitting modelling using the MSAT
+% toolset. It predicts the SKS splitting variation with backazimuth associated 
+% with two dipping, partially-aligned olivine layers. The splitting in each layer
+% is calculated using the Christoffel equation (MS_phasvels), and combined using
+% N-layer effective splitting equations (MS_effective_splitting_N; Silver and 
+% Savage, GJI, 1994). Straight raypaths through the upper mantle are assumed. 
+%
+% See also: MS_EFFECTIVE_SPLITTING_N, MS_PHASEVELS
+
+% (C) James Wookey and Andrew Walker, 2011
+
+function split_model()
+
+%  ** Setup model parameters
+      S_slow = 4.814 ; % (SKS at 100 degrees from iasp91) ->
+      aoi = 11.2262 ; % at 100 km depth, angle of incidence
+
+%  ** Layer 1 (upper) parameters
+      L1_depth = 0. ; L1_thick = 100. ; L1_dip = 0.0 ; % layer geometry
+      L1_aaz = 30.0 ; % a-axis azimuth (rel. to down dip direction)
+      L1_faln = 0.3 ; % fraction aligned
+      
+%  ** Layer 2 (lower) parameters
+      L2_depth = 150. ; L2_thick = 60. ; L2_dip = 30.0 ; % layer geometry
+      L2_aaz = 0.0 ;  % a-axis azimuth (rel. to down dip direction)
+      L2_faln = 0.3 ; % fraction aligned 
+
+      report_model(L1_depth, L1_thick, L1_dip, L1_aaz, L1_faln, ...
+                   L2_depth, L2_thick, L2_dip, L2_aaz, L2_faln) ; 
+
+%  ** imaging parameters
+      baz = [0:1:360] ; % 0 is down dip direction (perp. to strike)
+      inc = -ones(size(baz)).*90 + aoi ;
+
+%  ** calculate distances
+      [dist1]=distance_in_dipping_layer(L1_dip,aoi,L1_thick,baz) ;
+      [dist2]=distance_in_dipping_layer(L2_dip,aoi,L2_thick,baz) ;
+      
+%  ** load anisotropy, and generate an isotropic version of it. 
+      [Cani,rh] = MS_elasticDB('olivine') ;
+      [Ciso] = MS_decomp(MS_axes(Cani)) ;
+      Cani = MS_rot3(Cani,90,0,0) ; % orientation for dry upper mantle
+
+%  ** generate layer elasticities:
+%        This is a Voigt-Reuss-Hill average of the appropriately rotated olivine
+%        tensor and its isotropic equivalent.
+      [L1_C,~] = MS_VRH([L1_faln 1-L1_faln],...
+         MS_rot3(Cani,0,-L1_dip,L1_aaz,'order',[3 2 1]),rh, Ciso, rh) ;
+
+      [L2_C,~] = MS_VRH([L2_faln 2-L2_faln],...
+         MS_rot3(Cani,0,-L2_dip,L2_aaz,'order',[3 2 1]),rh, Ciso, rh) ;
+
+%  ** interrogate elasticities to generate splitting parameters for each layer.
+      [ pol, ~, vs1, vs2, ~, ~, ~ ] = MS_phasevels( L1_C, rh, inc, baz ) ;   
+      fast1 = MS_unwind_pm_90((baz - pol')) ; % geog. reference frame
+      tlag1 = dist1./vs2' - dist1./vs1' ;
+            
+      [ pol, ~, vs1, vs2, ~, ~, ~ ] = MS_phasevels( L2_C, rh, inc, baz ) ;   
+      fast2 = MS_unwind_pm_90((baz - pol')) ; % geog. reference frame
+      tlag2 = dist2./vs2' - dist2./vs1' ;
+
+%  ** calculate the effective splitting between 2 layers    
+      fast_eff = zeros(size(baz)) ;
+      tlag_eff = fast_eff ;
+      for i = 1:length(baz)
+         [fast_eff(i),tlag_eff(i)] = ...
+            MS_effective_splitting_N(0.125,baz(i), ...
+            [fast2(i) fast1(i)],[tlag2(i) tlag1(i)]) ;
+      end
+
+%  ** make a figure of the results
+      scrsz = get(0,'ScreenSize');
+      figure('Position',[1 scrsz(4) scrsz(3)/3 scrsz(4)*0.9]) ;
+
+%  ** lag times
+      subplot(2,1,1)
+      plot(baz,tlag1,'r--') ; hold on
+      plot(baz,tlag2,'g--') ;
+      plot(baz,tlag_eff,'k-','LineWidth',1.5)
+      axis([0 360 0 4])
+      xlabel('Polarisation (relative to downdip direction)')
+      ylabel('Lag times (s)')
+      legend('Upper layer','Lower layer','Total') ;
+
+%  ** fast directions
+      subplot(2,1,2)
+      plot(baz,fast1,'r--') ; hold on
+      plot(baz,fast2,'g--') ;
+      plot(baz,fast_eff,'k-','LineWidth',1.5)
+      axis([0 360 -90 90])
+      xlabel('Polarisation (relative to downdip direction)')
+      ylabel('Fast shear-wave orientation (degree)')      
+      legend('Upper layer','Lower layer','Total') ;
+      
+return
+
+function [dist]=distance_in_dipping_layer(dip,aoi,thick,baz)
+
+%  ** calculate apparent dip
+      alp = atand(tand(dip) .* sind(baz+90)) ;
+
+%  ** calculate distances
+      gam = 90 - alp - aoi ;
+      bet = 90 + alp ;
+      dist = thick .* sind(bet) ./ sind(gam) ;
+return
+
+function report_model(L1_depth, L1_thick, L1_dip, L1_aaz, L1_faln, ...
+                      L2_depth, L2_thick, L2_dip, L2_aaz, L2_faln) 
+
+%  ** Banner
+      fprintf('\n** SKS Shear-wave splitting in two layer anisotropic models.\n\n')
+
+%  ** Report layer details
+      fprintf('Layer 1: depth to top = %5.2fkm, thickness = %5.2f km, dip = %6.2f deg.\n', ...
+         L1_depth, L1_thick, L1_dip)
+      fprintf('         anisotropy: a-axis azimuth = %6.2f deg, v.f. aligned  = %4.1f\n', ...
+         L1_aaz,L1_faln)
+      fprintf('\n')
+
+      fprintf('Layer 2: depth to top = %5.2fkm, thickness = %5.2f km, dip = %6.2f deg.\n', ...
+         L2_depth, L2_thick, L2_dip)
+      fprintf('         anisotropy: a-axis azimuth = %6.2f deg, v.f. aligned  = %4.1f\n', ...
+         L2_aaz,L2_faln)
+
+      fprintf('\n** Plotting backazimuthal variation of splitting parameters ...\n')
+
+
+return
---
+++ examples/texture/texture_example.m
@@ -0,0 +1,116 @@
+% TEXTURE_EXAMPLE.M - Example script calculating elastic constants of a
+%                     textured rock as measured by EBSD.
+%
+% This script demonstrates how to combine MTEX and MSAT to calculate the
+% composite elastic constants of a rock consisiting of two phases where the
+% crystals are partially alligned. Based on the MTEX Aachen EBSD example we
+% read the EBSD data and assume that each data point represents a equal
+% volume of the rock. MTEX is used to convert the EBSD measurment into a
+% list of Euler angles. The MSAT function MS_rotEuler is then used to build
+% a list of elastic constants tensors aligned with each measured crystal 
+% orentation. Finally the tensors are averaged using MS_VRH and the seismic 
+% properties of the sample reported. 
+%
+% For this script to work both MSAT and MTEX must be installed. It is worth
+% noting that recent version of MTEX are able to calculate the composite
+% elastic constants from an orientation distribution function (ODF) without
+% having to sample a finite number of orientations. If the source data is
+% from an EBSD run this implies the need to first create an ODF from the
+% discrete orientation measurments. 
+%
+% See also: MS_VRH MS_ROTEULER LOADEBSD EULER 
+
+% (C) James Wookey and Andrew Walker, 2011
+
+
+function texture_example()
+
+    fprintf('\nMSAT TEXTURE EXAMPLE SCRIPT\n\n');
+
+    % Setup basic parameters - single crystal elastic constants and 
+    % symmetry. Note that I don't know what the the sample is made of -
+    % just that it's a two phase mixture with both phases belonging to the 
+    % m-3m point group. I'm going to guess a NaCl / KCl mixture. 
+    
+    % Elastic constants and density
+    [C1, rho1] = MS_elasticDB('NaCl');
+    [C2, rho2] = MS_elasticDB('KCl');
+    
+    % crystal symmetry
+    CS = {symmetry('m-3m'),...
+          symmetry('m-3m')};
+      
+    % specimen symmetry
+    SS = symmetry('-1');
+
+    
+    % Load in all the EBSD data using MTEX. NaCl data ends up in ebsd(1)
+    % and KCl data ends up in ebsd(2). 
+    tic; fprintf('Loading EBSD data ...');
+    
+    % specify file name
+    fname = fullfile(mtexDataPath,'aachen_ebsd','85_829grad_07_09_06.txt');
+    
+    % create an EBSD variable containing the data
+    ebsd = loadEBSD(fname,CS,SS,'interface','generic' ...
+        , 'ColumnNames', { 'Index' 'Phase' 'x' 'y' 'Euler1' 'Euler2' ...
+          'Euler3' 'MAD' 'BC' 'BS' 'Bands' 'Error' 'ReliabilityIndex'}, ...
+          'Bunge', 'ignorePhase', 0);
+
+    telap = toc; fprintf(' done (%4.2f secs)\n',telap);
+
+    
+    % For each EBSD measurment, create an elastic matrix with the measured 
+    % orentation. Work on NaCl, then KCl before joining the elastic
+    % matricies together
+    
+    tic; fprintf('Extracting Euler angles ...');
+    % NaCl Euler angles
+    [Nphi1s, Nthetas, Nphi2s] = Euler(get(ebsd(1),'orientations'), 'Bundge');
+    % KCl Euler angles
+    [Kphi1s, Kthetas, Kphi2s] = Euler(get(ebsd(2),'orientations'), 'Bundge');
+    telap = toc; fprintf(' done (%4.2f secs)\n',telap);
+  
+    % rads to deg...
+    
+    Nphi1s=Nphi1s*(180.0/pi); Kphi1s=Kphi1s*(180.0/pi);
+    Nthetas=Nthetas*(180.0/pi); Kthetas=Kthetas*(180.0/pi);
+    Nphi2s=Nphi2s*(180.0/pi); Kphi2s=Kphi2s*(180.0/pi);
+    num_nacl = length(Nphi1s); num_kcl = length(Kphi1s);
+    num_xtals = length(Nphi1s) + length(Kphi1s);
+    fprintf('%i5 NaCl and %i5 KCl measurments.\n', num_nacl, num_kcl);
+    
+    % Rotate all elastic constants.
+    
+    tic; fprintf('Creating list of rotated NaCl elasticity matrices ...');
+    NaCl_Cs = MS_rotEuler(C1, Nphi1s, Nthetas, Nphi2s);
+    NaCl_rhos = ones(num_xtals,1)*rho1;
+    telap = toc; fprintf(' done (%4.2f secs)\n',telap);
+    
+    tic; fprintf('Creating list of rotated NaCl elasticity matrices ...');
+    KCl_Cs = MS_rotEuler(C2, Kphi1s, Kthetas, Kphi2s);
+    KCl_rhos = ones(num_xtals,1)*rho2;
+    telap = toc; fprintf(' done (%4.2f secs)\n',telap);
+
+    
+    % Build the input arguments for MS_VRH and calculate the
+    % avarage.
+    
+    tic; fprintf('Calculating VRH average matrix ...');
+    Cs = zeros(6,6,num_xtals);
+    Cs(:,:,1:num_nacl) = NaCl_Cs(:,:,:);
+    Cs(:,:,num_nacl+1:num_xtals) = KCl_Cs(:,:,:);
+    vfs = ones(num_xtals,1); % Same volume fraction for each point.
+    rhos = [NaCl_rhos KCl_rhos] ;
+    
+    [Cav, rhoav] = MS_VRH(vfs, Cs, rhos);
+    telap = toc; fprintf(' done (%4.2f secs)\n\n',telap);
+
+    
+    % Finally, plot the S-wave anisotropy of the rock sample and report 
+    % some data on elastic constants.
+    
+    MS_sphere(Cav, rhoav, 'S');
+    MS_info(Cav, rhoav);
+    fprintf('\ndone.\n\n');
+end
