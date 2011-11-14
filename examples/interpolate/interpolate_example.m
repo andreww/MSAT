c99f4594a5b0630e11d2f2126fbc7362318af7a5
Andrew Walker <andrew.walker@bristol.ac.uk>
2011-11-14 16:25:13 +0000

    Finish off interpolate example

--- examples/interpolate/interpolate_example.m
+++ examples/interpolate/interpolate_example.m
@@ -1,17 +1,56 @@
+% INTERPOLATE_EXAMPLE.m - example script showing two ways elasticity 
+%                         matricies can be interpolated in MSAT.
+%
+% Interpolation of elastic constants can be a difficult problem. In this
+% example we show how two approaches, both implemented in MSAT, give quite 
+% different results. We imagine a region where the elasticity changes 
+% smoothly from one described by single crystal olivine rotated 30 degrees 
+% clockwise around its c-axis to one described by single crystal enstatie 
+% rotated 30 degrees anticlockwise around its c-axis and seek to calculate 
+% the elasticity of a point mid-way between these limits. One (commonly 
+% used) approach is to take the average of each element of the two 
+% elasticity tensors or the average of each element of the two compliance 
+% tensors. This amounts to the Voigt, Reuss or Voigt-Reuss-Hill average 
+% available via MS_VRH. If the interpolation is between identical rotated 
+% tensors this approach is clearly sub-optimal and an alternative is 
+% available in via MS_interpolate. This function by finding a common 
+% orientation for the two tensors, using MS_axes, averaging the rotated 
+% tensors, then moving the rotated tensor into the correct orientation. 
+% This example script compares the two approaches.  
+% 
+% See also: MS_VRH, MS_INTERPOLATE
 
-% Set up values we 'know' 
-[C0, roh] = MS_elasticDB('olivine');
-C30 = MS_rot3(C0, 0, 0, 30);
-C60 = MS_rot3(C0, 0, 0, 60);
-C90 = MS_rot3(C0, 0, 0, 90);
-C120 = MS_rot3(C0, 0, 0, 120);
+% (C) James Wookey and Andrew Walker, 2011
 
-% This does not work:
-C_interp_90 = MS_VRH([0.8, 0.2], C120, roh, C0, roh);
+function interpolate_example()
 
-[Cx90 RR90] = MS_axes(C90)
-[Cx120 RR120] = MS_axes(C120)
-[Cx0 RR0] = MS_axes(C0)
-[Cx30 RR30] = MS_axes(C30)
-[Cx60 RR60] = MS_axes(C60)
+    % Set up and plot values we 'know' - on one end we have rotated 
+    % olivine, on the other we have rotated enstatite
+    [C0_ol, rh_ol] = MS_elasticDB('olivine');
+    C30_ol = MS_rot3(C0_ol, 0, 0, 30);
+    MS_plot(C30_ol, rh_ol, 'wtitle', '100% olivine', 'quiet');
+    fprintf(['\n100%%' ' olivine\n'])
+    MS_info(C30_ol, rh_ol);
+    
+    [C0_en, rh_en] = MS_elasticDB('enstatite');
+    C30_en = MS_rot3(C0_en, 0, 0, -30);
+    MS_plot(C30_en, rh_en, 'wtitle', '100% enstatite', 'quiet');
+    fprintf(['\n100%%' ' enstatite\n'])
+    MS_info(C30_en, rh_en);
+    
+    % Do the VRH (element-wise) mean.
+    [C0_vrh, rh_vrh] = MS_VRH([0.5 0.5], C30_ol, rh_ol, C30_en, rh_en);
+    MS_plot(C0_vrh, rh_vrh, 'wtitle', ...
+        '50% olivine 50% enstatite (VRH)', 'quiet');
+    fprintf(['\n50%%' ' VRH mean\n'])
+    MS_info(C0_vrh, rh_vrh);
+    
+    % Do the MS_interpolate interpolation
+    C0_int = MS_interpolate(C30_ol, C30_en, 0.5);
+    MS_plot(C0_int, rh_vrh, 'wtitle', ...
+        '50% olivine 50% enstatite (common orientation)', 'quiet');
+    fprintf(['\n50%%' ' common orientation mean\n'])
+    MS_info(C0_int, rh_vrh);
+    
+end
 
