60a8148ce5c13dcfd54c74895d613699639c53ee
James Wookey <j.wookey@bristol.ac.uk>
2013-05-13 13:47:11 +0100

    Added optional weight output to split_misfit. Still need to implement something useful for this.

--- msat/MS_splitting_misfit.m
+++ msat/MS_splitting_misfit.m
@@ -5,16 +5,29 @@
 %
 % Usage: 
 %     [misfit] = MS_splitting_misfit(fast1,tlag1,fast2,tlag2,spol,dfreq)         
-%         Calculate the misfit between splitting operators [fast1,tlag1]
-%         and [fast2,tlag2], using the default mode (see below). spol is
-%         the initial source polarisation, dfreq is the dominant frequency.
+%        Calculate the misfit between splitting operators [fast1,tlag1]
+%        and [fast2,tlag2], using the default mode (see below). SPOL is
+%        the initial source polarisation, dfreq is the dominant frequency.
 %
+%     [misfit, weight] = MS_splitting_misfit(...)   
+%        As above, except an appropriate weight is also estimated for 
+%        the measurement. This does not apply to all modes; where it is
+%        not applicable it will be given the value 1.0. 
+%
+%        The weight ...
 %
 % Modes of operation:
-%     [misfit] = MS_splitting_misfit(...,'mode','lam2')
+%     [...] = MS_splitting_misfit(...,'mode','lam2')
 %        Calculate misfit based on the residual second eigenvalue of applying
 %        [f1,t1] to a test wavelet, then removing [f2,t2]. [DEFAULT].
 %
+%     [...] = MS_splitting_misfit(...,'mode','lam2S')
+%        Calculate misfit based on the residual second eigenvalue of applying
+%        [f1,t1] to a test wavelet, then removing [f2,t2]. This is then 
+%        reversed, and the final misfit is the average of the two. 
+%
+%     [...] = MS_splitting_misfit(...,'mode','simple')
+%        A simple (normalised) arithmetic misfit. 
 %
 % See also: MS_EFFECTIVE_SPLITTING_N
 
@@ -53,58 +66,112 @@
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 %===============================================================================
-function [misfit] = MS_splitting_misfit(fast1,tlag1,...
+function [varargout] = MS_splitting_misfit(fast1,tlag1,...
                                         fast2,tlag2,spol,dfreq,varargin) ;
 %===============================================================================
 
    % default mode
    modeStr = 'lam2' ;
+   idebug = 0 ;
+   max_tlag = NaN ; % autoselect max tlag
    
    % process the optional arguments
    iarg = 1 ;
    while iarg <= (length(varargin))
       switch lower(varargin{iarg})
+      case 'debug'  % parameter definition (value required)
+         idebug = 1 ;
+         iarg = iarg + 1 ;
       case 'mode'  % parameter definition (value required)
          modeStr = varargin{iarg+1} ;
          iarg = iarg + 2 ;
+      case 'max_tlag'  % parameter definition (value required)
+         max_tlag = varargin{iarg+1} ;
+         iarg = iarg + 2 ;         
       otherwise 
          error('MS:SPLITTING_MISFIT:UnknownOption',...
             ['Unknown option: ' varargin{iarg}]) ;   
       end
    end
+   
+   % check outputs
+   if nargout>2
+      error('MS:SPLITTING_MISFIT:TooManyOutputs','Too many outputs specified')
+   end
+   
 
    % call the appropriate mode function
    switch lower(modeStr)
+   case 'simple'
+      [misfit, weight]=MS_splitting_misfit_simple(fast1,tlag1,fast2,tlag2,spol,dfreq) ;      
    case 'lam2'
-      misfit=MS_splitting_misfit_lam2(fast1,tlag1,fast2,tlag2,spol,dfreq) ;
+      [misfit, weight]=MS_splitting_misfit_lam2( ...
+         fast1,tlag1,fast2,tlag2,spol,dfreq,max_tlag,idebug) ;
    case 'lam2s'
-      misfit=MS_splitting_misfit_lam2S(fast1,tlag1,fast2,tlag2,spol,dfreq) ;
+      [misfit, weight]=MS_splitting_misfit_lam2S( ...
+         fast1,tlag1,fast2,tlag2,spol,dfreq,max_tlag,idebug) ;
    otherwise
       error('MS:SPLITTING_MISFIT:UnknownMode',...
          ['Unknown mode: ' modeStr]) ;      
    end
    
+   % assign outputs
+   switch nargout
+      case 1
+         varargout = {misfit} ;
+      case 2
+         varargout = {misfit, weight} ;
+   end
+      
+   
 end
 %===============================================================================
 
 %===============================================================================
-function [misfit] = MS_splitting_misfit_lam2(fast1,tlag1,fast2,tlag2,spol,dfreq)
+function [misfit,weight] = ...
+   MS_splitting_misfit_simple(fast1,tlag1,fast2,tlag2,spol,dfreq)
+%===============================================================================
+% Simple numerical misfit. Average of normalised fast angular difference and 
+% time lag. TLAG misfit is normalised by half the dominant period. Also,
+% apply a weighting factor for the polarisation.
+
+   
+
+   fast_misfit = abs(MS_unwind_pm_90(fast1-fast2))./90 ;
+   tlag_misfit = abs(tlag1-tlag2)./(0.25/dfreq) ;
+   
+% polarisation weighting factor. Function which is maximal at 45 degrees.
+% sum of two functions? 
+   
+   misfit = mean([fast_misfit tlag_misfit]) ;
+   
+   weight = 1.0 ;
+   
+end
+%===============================================================================
+
+%===============================================================================
+function [misfit,weight] = ...
+   MS_splitting_misfit_lam2(fast1,tlag1,fast2,tlag2,spol,dfreq,max_tlag,idebug)
 %===============================================================================
 
    % generate a test wavelet.
+   if isnan(max_tlag), max_tlag = max([tlag1 tlag2]); , end
    
-   
-   [time,T00,T90] = MS_make_trace(spol,dfreq,max([tlag1 tlag2])) ;
+   [time,T00,T90] = MS_make_trace(spol,dfreq,max_tlag) ;
+
+   if (idebug), plot_wavelet(time,T00,T90), end
 
    % apply splitting operator 1 
    [T00sp,T90sp] = MS_split_trace(time,T00,T90,fast1,tlag1) ;
+
    
-   %plot_wavelet(time,T00sp,T90sp)
+   if (idebug), plot_wavelet(time,T00sp,T90sp), end
    
    % apply inverse of splitting operator 2
    [T00sp2,T90sp2] = MS_split_trace(time,T00sp,T90sp,fast2,-tlag2) ;   
 
-   %plot_wavelet(time,T00sp2,T90sp2)
+   if (idebug), plot_wavelet(time,T00sp2,T90sp2), end
    
    
    %% measure second eignvalue of the resulting wavelet
@@ -116,22 +183,26 @@ function [misfit] = MS_splitting_misfit_lam2(fast1,tlag1,fast2,tlag2,spol,dfreq)
       
    % calculate normalised misfit.   
    misfit = min([D(1,1) D(2,2)])./ max([D(1,1) D(2,2)]) ;   
+   weight = 1.0 ;
    
 end
 %===============================================================================
 
 %===============================================================================
-function [misfit] = MS_splitting_misfit_lam2S(fast1,tlag1,fast2,tlag2,spol,dfreq)
+function [misfit,weight] = ...
+   MS_splitting_misfit_lam2S(fast1,tlag1,fast2,tlag2,spol,dfreq,max_tlag,idebug)
 %===============================================================================
 
 % foward 
 
    % generate a test wavelet.
-   [time,T00,T90] = MS_make_trace(spol,dfreq,max([tlag1 tlag2])) ;
+   if isnan(max_tlag), max_tlag = max([tlag1 tlag2]); , end
+
+   [time,T00,T90] = MS_make_trace(spol,dfreq,max_tlag) ;
 
    % apply splitting operator 1 
    [T00sp,T90sp] = MS_split_trace(time,T00,T90,fast1,tlag1) ;
-      
+
    % apply inverse of splitting operator 2
    [T00sp2,T90sp2] = MS_split_trace(time,T00sp,T90sp,fast2,-tlag2) ;   
    
@@ -147,13 +218,13 @@ function [misfit] = MS_splitting_misfit_lam2S(fast1,tlag1,fast2,tlag2,spol,dfreq
 
    %% reverse
    
-   [time,T00,T90] = MS_make_trace(spol,dfreq,max([tlag1 tlag2])) ;
+   [time,T00,T90] = MS_make_trace(spol,dfreq,max_tlag) ;
 
    % apply splitting operator 2 
    [T00sp,T90sp] = MS_split_trace(time,T00,T90,fast2,tlag2) ;
       
    % apply inverse of splitting operator 1
-   [T00sp2,T90sp2] = MS_split_trace(time,T00sp,T90sp,fast1,-tlag1) ;   
+   [T00sp2,T90sp2] = MS_split_trace(time,T00sp,T90sp,fast1,-tlag1) ;  
    
    %% measure second eignvalue of the resulting wavelet
    % calculate the covariance matrix
@@ -167,6 +238,8 @@ function [misfit] = MS_splitting_misfit_lam2S(fast1,tlag1,fast2,tlag2,spol,dfreq
    
 %  result is the average of the two
    misfit = (misfit1+misfit2)/2 ;
-
+   weight = 1.0 ;
    
 end
+%===============================================================================
+
