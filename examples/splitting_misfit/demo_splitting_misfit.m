487689c9055b0d9ccb10c9cd137b0c200acb5fec
James Wookey <j.wookey@bristol.ac.uk>
2013-05-14 13:52:28 +0100

    * Added a code to demonstrate the different misfit functions implemented   in MS_split_misfit.m.

--- examples/splitting_misfit/demo_splitting_misfit.m
+++ examples/splitting_misfit/demo_splitting_misfit.m
@@ -4,30 +4,33 @@ function demo_splitting_misfit()
 
 
 fast_ref = 0 ;
-tlag_ref = 3 ;
-%fast = 60 ;
-%tlag = 3 ;
-%
-%MISFIT = ...
-%   MS_splitting_misfit(fast,tlag, ....
-%      fast_ref,tlag_ref,45,0.1,'mode','lam2','debug') 
-%
-%MISFIT = ...
-%   MS_splitting_misfit(fast_ref,tlag_ref, ...
-%      fast,tlag,45,0.1,'mode','lam2','debug') 
-
-%return
+tlag_ref = 2 ;
 
 
-figure
+figure('Position',[1 1 800 800]) ;
+
+fprintf('Yellow diamond is the reference value.\n')
+
 
 calc_misfit_surface(fast_ref,tlag_ref,-45,'lam2',0,1) ;
+hold on
+plot(tlag_ref,fast_ref,'kd','MarkerFaceColor','y','MarkerSize',12)
+title('Mode = lam2')
 
-calc_misfit_surface(fast_ref,tlag_ref,-45,'lam2',1,2) ;
+calc_misfit_surface(fast_ref,tlag_ref,45,'lam2S',0,2) ;
+hold on
+plot(tlag_ref,fast_ref,'kd','MarkerFaceColor','y','MarkerSize',12)
+title('Mode = lam2S')
 
-%calc_misfit_surface(fast_ref,tlag_ref,45,'lam2S',0,3) ;
+calc_misfit_surface(fast_ref,tlag_ref,-45,'simple',0,3) ;
+hold on
+plot(tlag_ref,fast_ref,'kd','MarkerFaceColor','y','MarkerSize',12)
+title('Mode = simple')
 
-%calc_misfit_surface(fast_ref,tlag_ref,45,'simple',0,4) ;
+calc_misfit_surface(fast_ref,tlag_ref,45,'intensity',0,4) ;
+hold on
+plot(tlag_ref,fast_ref,'kd','MarkerFaceColor','y','MarkerSize',12)
+title('Mode = intensity')
 
 
 end
@@ -55,9 +58,11 @@ function []=calc_misfit_surface(fast_ref,tlag_ref,spol,modeStr,reverse,ip) ;
       end
    end
 
-   subplot(1,2,ip)
-   contourf(TLAG,FAST,MISFIT,20) ; colorbar ;
-   
+   subplot(2,2,ip)
+   [C, H] = contourf(TLAG,FAST,MISFIT,20) ; colorbar ;
+   set(H,'LineStyle','none') ;
+   xlabel('TLAG (sec)')
+   ylabel('FAST (deg)')
 
 
 end
