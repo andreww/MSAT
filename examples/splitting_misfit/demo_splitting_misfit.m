ecdc757a48b19ed34d9571d91357bb0d305abad8
James Wookey <j.wookey@bristol.ac.uk>
2013-05-16 14:07:28 +0100

    * Splitting misfit example.
    - Added decent comments to the misfit example.

--- examples/splitting_misfit/demo_splitting_misfit.m
+++ examples/splitting_misfit/demo_splitting_misfit.m
@@ -1,6 +1,51 @@
-function demo_splitting_misfit()
+% DEMO_SPLITTING_MISFIT
 % Function to demonstrate the misfit functions implemented in 
 % MSAT to compare shear-wave splitting operator pairs. 
+%
+% This code plots a range of contour plots showing the
+% misfit between a reference splitting operator and a range
+% of other splitting operators, for different source
+% polarisations and misfit modes in MS_SPLITTING_MISFIT.
+%
+% See also: MS_SPLITTING_MISFIT
+
+% Copyright (c) 2013, James Wookey and Andrew Walker
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, 
+% with or without modification, are permitted provided 
+% that the following conditions are met:
+% 
+%    * Redistributions of source code must retain the 
+%      above copyright notice, this list of conditions 
+%      and the following disclaimer.
+%    * Redistributions in binary form must reproduce 
+%      the above copyright notice, this list of conditions 
+%      and the following disclaimer in the documentation 
+%      and/or other materials provided with the distribution.
+%    * Neither the name of the University of Bristol nor the names 
+%      of its contributors may be used to endorse or promote 
+%      products derived from this software without specific 
+%      prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS 
+% AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+% WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+% THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY 
+% DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+% USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+% OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+%==============================================================================
+function demo_splitting_misfit()
+%==============================================================================
 
 
 fast_ref = 0 ;
@@ -32,8 +77,12 @@ figure('Position',[1 1 800 800],'Name','Polarisation = 0') ;
    calc_misfit_surface(fast_ref,tlag_ref,0,'intensity',0,4) ;
 
 end
+%==============================================================================
 
+
+%==============================================================================
 function []=calc_misfit_surface(fast_ref,tlag_ref,spol,modeStr,reverse,ip) ;
+%==============================================================================
 
    fast = [-90:5:90] ;
    tlag = [0:0.1:4] ;
@@ -68,3 +117,4 @@ function []=calc_misfit_surface(fast_ref,tlag_ref,spol,modeStr,reverse,ip) ;
    title(['mode = ' modeStr]) ;
 
 end
+%==============================================================================
