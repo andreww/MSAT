7b9b063c5d596fb89c414e4b59ce7a8906e5667e
Andrew Walker <andrew.walker@bristol.ac.uk>
2013-07-14 20:37:13 +0100

    Start work on anisotropic reflection coeffs
    This needs lots of work!

---
+++ msat/MS_reflect.m
@@ -0,0 +1,14 @@
+% MS_REFLECT - Anisotropic Reflection and transmission coefficients
+
+function [rP, rS1, rS2, tP, tS1, tS2] = ... 
+    MS_reflect(C1, rh1, C2, rh2, inc, azi)
+
+
+    a1 = C1/rh1; % Density normalised, but what units
+    a2 = C2/rh2; % and do we need this anyway...
+    
+    % Xs in eq. 3
+    
+    
+    
+end
